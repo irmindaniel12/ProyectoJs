a8df6a9eac24ff2fa741e80ce2d9c3321a0c45d0
Irmin Daniel <irmin12julio@gmail.com>
2021-01-13 11:14:13 -0600

    Login con LocalStorage

--- js/main.js
+++ js/main.js
@@ -70,4 +70,26 @@ $(document).ready(function(){
 
         return false;
       });
+
+      //Login false
+      $("#login form").submit(function(){
+        var form_name=$("#form_name").val();
+        localStorage.setItem("form_name", form_name);
+      });
+
+      var form_name = localStorage.getItem("form_name");
+
+      if(form_name !=null && form_name != "undefined"){
+          var about_parrafo= $("#about p");
+          about_parrafo.html("<br><strong>Bienvenido, "+ form_name+"</strong>");
+          about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a");
+
+        $("#login").hide();
+
+        $("#logout").click(function(){
+            localStorage.clear();
+            location.reload();
+        });
+      }
+      
 });
