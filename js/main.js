4c0990cb0072f6e526bfde77d36968ecc4933ef0
Irmin Daniel <irmin12julio@gmail.com>
2021-01-13 12:20:28 -0600

    Formulario de contacto con validación

--- js/main.js
+++ js/main.js
@@ -111,4 +111,20 @@ $(document).ready(function(){
         },1000);
         
       }
+
+      //Validación
+
+      if(window.location.href.indexOf('contact') >-1){
+        
+        $("form input[name='date']").datepicker({
+            dateFormat: 'dd-mm-yy'
+        });
+
+        $.validate({
+            lang:'es',
+            errorMessagePosition: 'top',
+            scrollToTopOnError:true
+        });
+        
+      }
 });
